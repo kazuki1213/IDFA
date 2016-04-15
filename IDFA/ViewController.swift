f40465a65fdb4b69591e5ec9a47d1a456b865245
野澤 一貴 <kazuki.nozawa@mbp.intra.emercury.co.jp>
2016-04-15 16:01:56 +0900

    Initial Commit

---
+++ IDFA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IDFA
+//
+//  Created by 野澤 一貴 on 2016/04/15.
+//  Copyright © 2016年 Kazuki Nozawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
